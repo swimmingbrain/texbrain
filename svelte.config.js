1af29b9f7f06e0b9ca4aee692bd6e4b5a700bc8e
swimmingbrain <braianplaku@gmail.com>
2026-03-21 10:59:47 +0100

    fix base path for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,10 @@ const config = {
       fallback: 'index.html',
       precompress: false,
       strict: true
-    })
+    }),
+    paths: {
+      base: process.env.GITHUB_ACTIONS ? '/texbrain' : ''
+    }
   }
 };
 
